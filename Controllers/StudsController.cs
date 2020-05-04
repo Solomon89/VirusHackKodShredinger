cbb5b8d7bb33e21e867fd37b10cbac1e0c84b65d
Дмитрий Владимирович Соломонов <solomon89@list.ru>
2020-05-04 15:30:14 +0300

    face detector

--- Controllers/StudsController.cs
+++ Controllers/StudsController.cs
@@ -16,5 +16,9 @@ namespace VirusHackKodShredinger.Controllers
         {
             return View();
         }
+        public IActionResult CheckEyes()
+        {
+            return PartialView();
+        }
     }
 }
