007e76bbbee9510bc56602ccd362d9d10a5257d1
ioaznis <ioaznis@git>
2020-05-04 17:31:44 +0300

    Session state

---
+++ Repository/Session.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VirusHackKodShredinger.Repository
+{
+    public class Session
+    {
+        public long Id { get; set; }
+        public long JobId { get; set; }
+        public long StudentId { get; set; }
+        
+        public DateTime TimeStamp { get; set; }
+
+        public int State { get; set; }
+    }
+}
